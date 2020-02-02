fd24086d2877487ddbda52eeede65710977fb056
Marcin Morawski <marcin@morawskim.pl>
2020-02-02 16:21:59 +0100

    Picsum provider to generate fake images
    Faker include support for Lorempixel, but currently this service is down
    (fzaninotto/Faker#1884).
    We can't use $faker->image due to timeouts.
    I created a new provider (picsum) to fetch or generate url to random images.

---
+++ src/PicsumProvider.php
@@ -0,0 +1,102 @@
+<?PHP
+
+namespace Mmo\Faker;
+
+use Faker\Provider\Base as BaseProvider;
+
+class PicsumProvider extends BaseProvider
+{
+    public static function picsumUrl($width = 640, $height = 480, $id = null, $randomize = true, $gray = false, $blur = null)
+    {
+        $baseUrl = 'https://picsum.photos/';
+        $url = '';
+        if ($id) {
+            $url = 'id/' . $id;
+        }
+        $url .= "{$width}/{$height}";
+
+        $queryParams = array();
+        if ($gray) {
+            $queryParams['grayscale'] = '';
+        }
+
+        if ($blur) {
+            $queryParams['blur'] = '';
+        }
+
+        if ($randomize) {
+            $queryParams['random'] = static::randomNumber(5, true);
+        }
+
+        $queryString = '';
+        if (!empty($queryParams)) {
+            $queryString = '?' . http_build_query($queryParams);
+        }
+
+        return $baseUrl . $url . $queryString;
+    }
+
+    /**
+     * Download a remote random image to disk and return its location
+     *
+     * Requires curl, or allow_url_fopen to be on in php.ini.
+     *
+     * @example '/path/to/dir/13b73edae8443990be1aa8f1a483bc27.jpg'
+     */
+    public static function picsum($dir = null, $width = 640, $height = 480, $fullPath = true, $id = null, $randomize = true, $gray = false, $blur = null)
+    {
+        $url = static::picsumUrl($width, $height, $id, $randomize, $gray, $blur);
+        return self::fetchImage($url, $dir, $fullPath);
+    }
+
+    /**
+     * Download a remote random image to disk and return its location
+     *
+     * Requires curl, or allow_url_fopen to be on in php.ini.
+     *
+     * @param string $url Image url to fetch
+     * @param null|string $dir Directory where downloaded image will be stored
+     * @param bool $fullPath Return full path to file or only filename
+     * @return bool|\RuntimeException|string
+     */
+    private static function fetchImage($url, $dir = null, $fullPath = true)
+    {
+        $dir = $dir === null ? sys_get_temp_dir() : $dir; // GNU/Linux / OS X / Windows compatible
+        // Validate directory path
+        if (!is_dir($dir) || !is_writable($dir)) {
+            throw new \InvalidArgumentException(sprintf('Cannot write to directory "%s"', $dir));
+        }
+
+        // Generate a random filename. Use the server address so that a file
+        // generated at the same time on a different server won't have a collision.
+        $name = md5(uniqid(empty($_SERVER['SERVER_ADDR']) ? '' : $_SERVER['SERVER_ADDR'], true));
+        $filename = $name .'.jpg';
+        $filepath = $dir . DIRECTORY_SEPARATOR . $filename;
+
+        // save file
+        if (function_exists('curl_exec')) {
+            // use cURL
+            $fp = fopen($filepath, 'w');
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_FILE, $fp);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+            $success = curl_exec($ch) && curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200;
+            fclose($fp);
+            curl_close($ch);
+
+            if (!$success) {
+                unlink($filepath);
+
+                // could not contact the distant URL or HTTP error - fail silently.
+                return false;
+            }
+        } elseif (ini_get('allow_url_fopen')) {
+            // use remote fopen() via copy()
+            $success = copy($url, $filepath);
+        } else {
+            return new \RuntimeException('The image formatter downloads an image from a remote HTTP server. Therefore, it requires that PHP can request remote hosts, either via cURL or fopen()');
+        }
+
+        return $fullPath ? $filepath : $filename;
+    }
+}
---
+++ test/Faker/PicsumProviderTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Mmo\Faker\Test;
+
+use Mmo\Faker\PicsumProvider;
+use PHPUnit_Framework_TestCase;
+
+class PicsumProviderTest extends PHPUnit_Framework_TestCase
+{
+    public function testPicsumUrlUses640x680AsTheDefaultSize()
+    {
+        $this->assertRegExp('#^https://picsum.photos/640/480#', PicsumProvider::picsumUrl());
+    }
+
+    public function testPicsumUrlAcceptsCustomWidthAndHeight()
+    {
+        $this->assertRegExp('#^https://picsum.photos/800/400#', PicsumProvider::picsumUrl(800, 400));
+    }
+
+    public function testPicsumUrlWithBlur()
+    {
+        $this->assertRegExp('#^https://picsum\.photos/800/400\?blur=#', PicsumProvider::picsumUrl(800, 400, null, false, false, true));
+    }
+
+    public function testPicsumUrlGray()
+    {
+        $this->assertRegExp('#^https://picsum\.photos/800/400\?grayscale=#', PicsumProvider::picsumUrl(800, 400, null, false, true));
+    }
+
+    public function testPicsumUrlWithGrayAndBlur()
+    {
+        $imageUrl = PicsumProvider::picsumUrl(
+            800,
+            400,
+            null,
+            false,
+            true,
+            true
+        );
+
+        $this->assertSame('https://picsum.photos/800/400?grayscale=&blur=', $imageUrl);
+    }
+
+    public function testPicsumUrlAddsARandomGetParameterByDefault()
+    {
+        $url = PicsumProvider::picsumUrl(800, 400);
+        $splitUrl = explode('?', $url);
+
+        $this->assertEquals(count($splitUrl), 2);
+        $this->assertRegexp('#random=\d{5}#', $splitUrl[1]);
+    }
+
+    public function testPicsumDownloadWithDefaults()
+    {
+        $file = PicsumProvider::picsum(sys_get_temp_dir());
+        $this->assertFileExists($file);
+        if (function_exists('getimagesize')) {
+            list($width, $height, $type) = getimagesize($file);
+            $this->assertEquals(640, $width);
+            $this->assertEquals(480, $height);
+            $this->assertEquals(constant('IMAGETYPE_JPEG'), $type);
+        } else {
+            $this->assertEquals('jpg', pathinfo($file, PATHINFO_EXTENSION));
+        }
+        if (file_exists($file)) {
+            unlink($file);
+        }
+    }
+}
